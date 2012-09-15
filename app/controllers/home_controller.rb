afea38035a28d5838d3bfa68c9ddddef8226fe9d
hiro <hs.iarn3@gmail.com>
2012-09-15 13:24:58 +0900

    Adjust login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
+
+  skip_filter :check_logined, only: [:index]
+
+  # このアクションはログイン画面とホーム画面を兼ねている
   def index
-    #render :text =>"hogehoge"
+    # ログイン状態でビューの描画を切り分ける
+    if session[:user_id]
+      #@user = User.find(session[:user_id])
+      render 'index'
+    else
+      render 'login'
+    end
   end
+
+  def pdf
+    send_file("public/ozaken/f_1300778637_1.pdf")
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,80 +1,69 @@
 class SessionsController < ApplicationController
 
+  skip_filter :check_logined
+
   def callback #login
+
     #認証後のハッシュ値を受け取る
     auth = request.env['omniauth.auth']
 
     #ユーザーが既に登録されているかをチェックする
-    #userの中には、usersテーブルのレコードが一式格納されている →　Userオブジェクトという
     user = User.find_by_twitter_id("#{auth['uid']}")
 
     if user 
       #既にUsersテーブルに登録されている場合
-      session[:twitter_id] = user.twitter_id
       session[:user_id] = user.id
-      redirect_to root_url, :notice=>"Login OK!"
+      session[:twitter_id] = user.twitter_id
+      redirect_to root_url
     else 
-      #登録されていない場合
-      #ユーザー情報のDB登録
+      #登録されていない場合はユーザー情報のDB登録
       User.create_account(auth)
-  
-      #ここで、変数userにユーザー情報格納し、セッション変数にも格納しておかないと挙動がおかしくなる
+
+      #ユーザー情報をセッションへ格納
       user = User.find_by_twitter_id("#{auth['uid']}")
       session[:user_id] = user.id
-     
+      session[:twitter_id] = user.twitter_id
+
       #ルートにリダイレクト
-      redirect_to root_url, :notice=>"#{auth['info']['name']}'s Twitter Account get!"
+      redirect_to root_url 
     end
-  end
-
-
-  def fb_callback
-    #認証後のハッシュ値を受け取る
-    auth = request.env['omniauth.auth']
-    # raise auth.to_yaml
-    # exit
-    logger.debug(auth)
-
-    #ユーザーが既に登録されているかをチェックする
-    user = FbUser.find_by_fb_id("#{auth['uid']}")
-
-    if user 
-      #既にUsersテーブルに登録されている場合
-      session[:fb_id] = user.fb_id
-      session[:fbuser_id] = user.id
-      redirect_to root_url, :notice=>"FbLogin OK!"
-    else 
-      #登録されていない場合 → ユーザー情報のDB登録
-      FbUser.create_fbaccount(auth)
-  
-      #ここで、変数userにユーザー情報格納し、セッション変数にも格納しておかないと挙動がおかしくなる
-      user = FbUser.find_by_fb_id("#{auth['uid']}")
-      session[:fb_id] = user.fb_id
-      session[:fbuser_id] = user.id
-     
-      #ルートにリダイレクト
-      redirect_to root_url, :notice=>"#{auth['info']['name']}'s Facebook Account get!"
-    end
-
   end
 
 
   def destroy #logout
     #セッションを開放する
-    session[:twitter_id] = nil
-    session[:user_id] = nil
+    reset_session
 
     #ログイン画面にリダイレクトする
-    redirect_to root_url, :notice=> "sign out"
+    redirect_to root_url
+  end
+
+
+  def fb_callback
+
+    auth = request.env['omniauth.auth']
+    # OauthToken取得
+    logger.debug(auth)
+
+    user = FbUser.find_by_fb_id("#{auth['uid']}")
+
+    if user 
+      session[:fb_id] = user.fb_id
+      session[:fbuser_id] = user.id
+      redirect_to root_url
+    else 
+      FbUser.create_fbaccount(auth)
+      user = FbUser.find_by_fb_id("#{auth['uid']}")
+      session[:fb_id] = user.fb_id
+      session[:fbuser_id] = user.id
+      redirect_to root_url
+    end
+
   end
 
 
   def fb_destroy
-    #セッションを開放する
-    session[:fb_id] = nil
-    session[:fbuser_id] = nil
-
-    #ログイン画面にリダイレクトする
+    reset_session
     redirect_to root_url, :notice=> "sign out"
   end
 
