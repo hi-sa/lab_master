9f325550877d41389dcbb1409af3886c04920da0
hiro <hs.iarn3@gmail.com>
2012-10-02 01:11:34 +0900

    Develope modal-window for showing mails body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
   #end
 
+  def hbr(str)
+    str = html_escape(str)
+    str.gsub(/\r\n|\r|\n/, "<br />")
+  end
+
 end
