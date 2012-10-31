7bb251a3085753f3878af164b49c4a4dc7a8dde3
hiro <hs.iarn3@gmail.com>
2012-10-31 18:01:36 +0900

    Create group page

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,41 +1,42 @@
 # -*- coding: utf-8 -*-
 class GroupController < ApplicationController
-  # GET group_index
+
+  layout "twitter"
+
   def index
-    # ユーザーが作成したグループの取得
-    @groups = Group.find_all_by_user_id(session[:user_id])
-    #@group_members = Group.find(1).group_members 
-    #@hoge = GroupMembers.find(1)
+    @groups = Group.all
+    #@group_memebres = GroupMember.all
   end
 
-  def new
-    #formから入力された値を格納するオブジェクトの生成
-    @group = Group.new
-  end
-
-  def create
-    # この一行で、group..と名付けられた全ての入力値をハッシュで取得する
-    @group = Group.new(params[:group])
-    @group.attributes = {:user_id => session[:user_id]}
-
-    respond_to do |format|
-      if @group.save
-        #format.html { redirect_to(@group, :notice => 'Group was successfully created!')}
-        format.html { redirect_to(group_index_path, :notice => 'Group was successfully created!')}
-        format.xml { render :xml => @group, :status => :created, :location => @group}
-      else
-        format.html { render :action => 'new'}
-        format.xml { render :xml => @group.errors, :status => :unprocessable_enttity}
-      end
-    end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  private
+#  def new
+#    #formから入力された値を格納するオブジェクトの生成
+#    @group = Group.new
+#  end
+#
+#  def create
+#    # この一行で、group..と名付けられた全ての入力値をハッシュで取得する
+#    @group = Group.new(params[:group])
+#    @group.attributes = {:user_id => session[:user_id]}
+#
+#    respond_to do |format|
+#      if @group.save
+#        #format.html { redirect_to(@group, :notice => 'Group was successfully created!')}
+#        format.html { redirect_to(group_index_path, :notice => 'Group was successfully created!')}
+#        format.xml { render :xml => @group, :status => :created, :location => @group}
+#      else
+#        format.html { render :action => 'new'}
+#        format.xml { render :xml => @group.errors, :status => :unprocessable_enttity}
+#      end
+#    end
+#  end
+#
+#  def edit
+#  end
+#
+#  def update
+#  end
+#
+#  def destroy
+#  end
 end
