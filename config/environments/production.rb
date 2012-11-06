220a6baa4ba505768c9ad8538059714b489661c6
hiro <hs.iarn3@gmail.com>
2012-11-06 23:46:14 +0900

    Facebookでの予期しないメンバーのアクションとファイルページの元のメールを見るボタンの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ LabMaster::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
+  # ↓ change!!!
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
